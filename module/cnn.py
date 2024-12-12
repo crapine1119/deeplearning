ae33f4b80ac2f299f3a5558387d60b2b7c8b9f9e
songhak <crapine1119@gmail.com>
2024-12-13 01:49:10 +0900

    feat(add): img2col & CNN

--- module/cnn.py
+++ module/cnn.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+from util.img2col import img2col_tensor
+
 
 class CNN2DImplemented(nn.Module):
     def __init__(self, in_channel: int, out_channel: int, kernel_size: int = 3, pad_size: int = 0, stride: int = 1):
@@ -11,22 +13,24 @@ class CNN2DImplemented(nn.Module):
         self._kernel_size = kernel_size
         self._pad_size = pad_size
         self._stride = stride
-        self._weight = nn.Parameter(torch.randn((kernel_size, kernel_size)))
-        self._bias = nn.Parameter(torch.randn((kernel_size, kernel_size)))
+        self._weight = nn.Parameter(torch.randn((in_channel, kernel_size, kernel_size, out_channel)))
+        self._bias = nn.Parameter(torch.randn((out_channel, kernel_size, kernel_size)))
 
-    def _reshape_img(self, x: torch.Tensor) -> torch.Tensor:
+    def _pad_img(self, x: torch.Tensor) -> torch.Tensor:
         pad = (self._pad_size, self._pad_size, self._pad_size, self._pad_size, 0, 0, 0, 0)
-        return F.pad(x, pad)[..., :: self._stride, :: self._stride]
+        return F.pad(x, pad)
 
     def forward(self, x: torch.Tensor):
         assert len(x.shape) == 4, AssertionError(
             f"length of tensor should be (B,C,H,W), but current length is {len(x)}"
         )
-        x = self._reshape_img(x)
-        return
+        x = self._pad_img(x)
+        col = img2col_tensor(x, kernel_size=self._kernel_size, stride=self._stride)
+        col = torch.matmul(col, self._weight.reshape(-1, self._out_channel))
+        return torch.dot(col, self._weight.reshape(-1, self._out_channel))
 
 
-if __name__ is "__main__":
+if __name__ == "__main__":
     img = torch.rand(4, 3, 7, 7)
-    layer = CNN2DImplemented(16, 3, 1, 1)
+    layer = CNN2DImplemented(3, 16, 3, 1)
     layer(img)
--- util/img2col.py
+++ util/img2col.py
@@ -1,7 +1,8 @@
 import numpy as np
+import torch
 
 
-def img2col(x: np.ndarray, kernel_size: int = 3, stride: int = 1):
+def img2col(x: np.ndarray, kernel_size: int = 3, stride: int = 1) -> np.ndarray:
     n, c, h, w = x.shape
     out_h, out_w = (h - kernel_size) // stride + 1, (w - kernel_size) // stride + 1
 
@@ -13,4 +14,19 @@ def img2col(x: np.ndarray, kernel_size: int = 3, stride: int = 1):
 
             patch = x[..., row_start : row_end + 1 : stride, col_start : col_end + 1 : stride]  # N, C, p1, p2
             output[..., row_start, col_start] = patch.transpose(0, 2, 3, 1)
+    return output.reshape(n, out_h, out_w, -1)
+
+
+def img2col_tensor(x: torch.Tensor, kernel_size: int = 3, stride: int = 1) -> torch.Tensor:
+    n, c, h, w = x.shape
+    out_h, out_w = (h - kernel_size) // stride + 1, (w - kernel_size) // stride + 1
+
+    output = torch.zeros((n, out_h, out_w, c, kernel_size, kernel_size))
+    for row_start in range(kernel_size):
+        row_end = row_start + h - kernel_size
+        for col_start in range(kernel_size):
+            col_end = col_start + w - kernel_size
+
+            patch = x[..., row_start : row_end + 1 : stride, col_start : col_end + 1 : stride]  # N, C, p1, p2
+            output[..., row_start, col_start] = patch.permute(0, 2, 3, 1)
     return output.reshape(n, out_h, out_w, -1)
